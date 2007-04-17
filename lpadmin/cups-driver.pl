6132516154d78b102832ac917d080d73f6cddce8
Jamie Cameron <jcameron@webmin.com>
2007-04-17 17:25:06 +0000

    Some find versions don't support -L

--- lpadmin/cups-driver.pl
+++ lpadmin/cups-driver.pl
@@ -132,7 +132,9 @@ printf "<td><input name=program size=40 value='%s'></td> </tr>\n",
 printf "<tr> <td valign=top><input type=radio name=mode value=1 %s> %s</td>\n",
 	$_[1]->{'mode'} == 1 ? 'checked' : '', $text{'cups_driver'};
 local (@ppds, $d, $f, $ppd, %cache, $outofdate, @files, %donefile);
-open(FIND, "find -L ".quotemeta($config{'model_path'})." -type f -print |");
+local $findver = `find --version 2>&1`;
+local $flag = $findver =~ /GNU\s+find\s+version\s+4\.2/i ? "-L" : "";
+open(FIND, "find $flag ".quotemeta($config{'model_path'})." -type f -print |");
 while(<FIND>) {
 	chop;
 	/([^\/]+)$/;
