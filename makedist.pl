64580ce54930f4995e084e01f4fdfba633c0e849
Jamie Cameron <jcameron@webmin.com>
2007-04-18 21:55:09 +0000

    Remove .svn files from package

--- makedist.pl
+++ makedist.pl
@@ -141,7 +141,7 @@ closedir(DIR);
 
 # Remove useless .bak, test and other files, and create the tar.gz file
 print "Creating webmin-$vfile.tar.gz\n";
-system("find $tardir/$dir -name '*.bak' -o -name test -o -name '*.tmp' -o -name '*.site' -o -name core -o -name .xvpics | xargs rm -rf");
+system("find $tardir/$dir -name '*.bak' -o -name test -o -name '*.tmp' -o -name '*.site' -o -name core -o -name .xvpics -o -name .svn | xargs rm -rf");
 system("cd $tardir ; tar cvhf - $dir 2>/dev/null | gzip -c >webmin-$vfile.tar.gz");
 
 if (!$min && -d $zipdir) {
