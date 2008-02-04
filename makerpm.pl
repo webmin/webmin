f72e7c5f04eb382e381cb2c49480cf50cbdc85ff
Jamie Cameron <jcameron@webmin.com>
2008-02-04 19:56:46 +0000

    fixed temp path

--- makerpm.pl
+++ makerpm.pl
@@ -206,8 +206,8 @@ if [ "\$tempdir" = "" ]; then
 	tempdir=/tmp/.webmin
 fi
 export config_dir var_dir perl autoos port login crypt host ssl nochown autothird noperlpath nouninstall nostart allow atboot
-./setup.sh >$tempdir/webmin-setup.out 2>&1
-chmod 600 $tempdir/webmin-setup.out
+./setup.sh >\$tempdir/webmin-setup.out 2>&1
+chmod 600 \$tempdir/webmin-setup.out
 rm -f /var/lock/subsys/webmin
 if [ "$inetd" != "1" ]; then
 	/etc/init.d/webmin start >/dev/null 2>&1 </dev/null
