56eaa8aa490316dfe36f0b5c4f0dee33cbd254e3
Jamie Cameron <jcameron@webmin.com>
2008-03-13 17:00:52 +0000

    Moved SSLPassPhraseDialog to global

--- apache/mod_ssl.pl
+++ apache/mod_ssl.pl
@@ -8,7 +8,7 @@ $rv = [ [ 'SSLEngine', 0, 14, 'virtual', undef, 10 ],
 	[ 'SSLProtocol', 0, 14, 'virtual', undef, 10 ],
 	[ 'SSLCertificateFile', 0, 14, 'virtual', undef, 9 ],
 	[ 'SSLCertificateKeyFile', 0, 14, 'virtual', undef, 8 ],
-	[ 'SSLPassPhraseDialog', 0, 14, 'virtual', 2.0, 7.5 ],
+	[ 'SSLPassPhraseDialog', 1, 14, 'global', 2.0, 7.5 ],
 	[ 'SSLVerifyClient', 0, 14, 'virtual directory htaccess', undef, 7 ],
 	[ 'SSLVerifyDepth', 0, 14, 'virtual directory htaccess', undef, 6 ],
 	[ 'SSLLog', 0, 14, 'virtual', undef, 5 ],
@@ -120,70 +120,86 @@ return $in{'SSLRequireSSL'} ? ( [ "" ] ) : ( [ ] );
 
 sub edit_SSLPassPhraseDialog
 {
-local ($mode, $script, $pass, $file);
-if ($_[0]->{'value'} eq 'builtin') {
-	$mode = 1;
-	}
-elsif ($_[0]->{'value'} =~ /^exec:(.*)$/) {
-	$file = $1;
-	local $data = &read_file_contents($1);
-	if ($data =~ /^#!\/bin\/sh\necho\s(.*)\n$/) {
-		$pass = $1;
-		$mode = 2;
+local $table = &ui_columns_start();
+local $i = 0;
+foreach my $p (@{$_[0]}, { }) {
+	local ($mode, $script, $pass, $file);
+	if ($p->{'value'} eq 'builtin') {
+		$mode = 1;
+		}
+	elsif ($p->{'value'} =~ /^exec:(.*)$/) {
+		$file = $1;
+		local $data = &read_file_contents($1);
+		if ($data =~ /^#!\/bin\/sh\necho\s(.*)\n$/) {
+			$pass = $1;
+			$mode = 2;
+			}
+		else {
+			$script = $file;
+			$file = undef;
+			$mode = 3;
+			}
+		}
+	elsif ($p->{'value'}) {
+		$script = $p->{'value'};
+		$mode = 1;
 		}
 	else {
-		$script = $file;
-		$file = undef;
-		$mode = 3;
+		$mode = 0;
 		}
+	$table .= &ui_columns_row([
+		&ui_radio("SSLPassPhraseDialog_$i", $mode,
+			[ [ 0, $text{'mod_ssl_passnone'}."<br>" ],
+			  [ 1, $text{'mod_ssl_builtin'}."<br>" ],
+			  [ 2, &text('mod_ssl_passph',
+			     &ui_textbox("SSLPassPhraseDialog_pass_$i",
+					 $pass, 20))."<br>" ],
+			  [ 3, &text('mod_ssl_passsc', 
+			     &ui_textbox("SSLPassPhraseDialog_script_$i",
+					 $script, 40)) ],
+			])."\n".
+		&ui_hidden("SSLPassPhraseDialog_file_$i", $file)
+		]);
+	$i++;
 	}
-elsif ($_[0]->{'value'}) {
-	$script = $_[0]->{'value'};
-	$mode = 1;
-	}
-else {
-	$mode = 0;
-	}
-return (2, $text{'mod_ssl_pass'},
-	&ui_radio("SSLPassPhraseDialog", $mode,
-		[ [ 0, $text{'default'} ],
-		  [ 1, $text{'mod_ssl_builtin'}."<br>" ],
-		  [ 2, &text('mod_ssl_passph',
-		     &ui_textbox("SSLPassPhraseDialog_pass", $pass, 20))."<br>" ],
-		  [ 3, &text('mod_ssl_passsc', 
-		     &ui_textbox("SSLPassPhraseDialog_script", $script, 40)) ],
-		])."\n".
-	&ui_hidden("SSLPassPhraseDialog_file", $file));
+$table .= &ui_columns_end();
+return (2, $text{'mod_ssl_pass'}, $table);
 }
 sub save_SSLPassPhraseDialog
 {
-if ($in{'SSLPassPhraseDialog'} == 0) {
-	return ( [ ] );
-	}
-elsif ($in{'SSLPassPhraseDialog'} == 1) {
-	return ( [ "builtin" ] );
-	}
-elsif ($in{'SSLPassPhraseDialog'} == 2) {
-	$in{'SSLPassPhraseDialog_pass'} =~ /\S/ ||
-		&error($text{'mod_ssl_epassph'});
-	local $file = $in{'SSLPassPhraseDialog_file'} ||
-		"$config{'httpd_dir'}/passphrase.".time().".sh";
-	&open_tempfile(PASS, ">$file");
-	&print_tempfile(PASS, "#!/bin/sh\n");
-	&print_tempfile(PASS, "echo ",$in{'SSLPassPhraseDialog_pass'},"\n");
-	&close_tempfile(PASS);
-	&set_ownership_permissions(undef, undef, 0755, $file);
-	return ( [ "exec:$file" ] );
-	}
-elsif ($in{'SSLPassPhraseDialog'} == 3) {
-	if ($in{'SSLPassPhraseDialog_script'} =~ /^[a-z]+:/) {
-		return ( [ $in{'SSLPassPhraseDialog_script'} ] );
+local @rv;
+local $mode;
+for(my $i=0; defined($in{"SSLPassPhraseDialog_$i"}); $i++) {
+	if ($in{"SSLPassPhraseDialog_$i"} == 0) {
+		# Nothing to add
 		}
-	else {
-		$in{'SSLPassPhraseDialog_script'} =~ /^\/\S/ ||
-			&error($text{'mod_ssl_epasssc'});
-		return ( [ "exec:".$in{'SSLPassPhraseDialog_script'} ] );
+	elsif ($in{"SSLPassPhraseDialog_$i"} == 1) {
+		push(@rv, "builtin");
+		}
+	elsif ($in{"SSLPassPhraseDialog_$i"} == 2) {
+		$in{"SSLPassPhraseDialog_pass_$i"} =~ /\S/ ||
+			&error($text{'mod_ssl_epassph'});
+		local $file = $in{"SSLPassPhraseDialog_file_$i"} ||
+			"$config{'httpd_dir'}/passphrase.".time().".sh";
+		&open_tempfile(PASS, ">$file");
+		&print_tempfile(PASS, "#!/bin/sh\n");
+		&print_tempfile(PASS, "echo ",
+			$in{"SSLPassPhraseDialog_pass_$i"},"\n");
+		&close_tempfile(PASS);
+		&set_ownership_permissions(undef, undef, 0755, $file);
+		push(@rv, "exec:$file");
+		}
+	elsif ($in{"SSLPassPhraseDialog_$i"} == 3) {
+		if ($in{"SSLPassPhraseDialog_script_$i"} =~ /^[a-z]+:/) {
+			push(@rv, $in{"SSLPassPhraseDialog_script_$i"});
+			}
+		else {
+			$in{"SSLPassPhraseDialog_script_$i"} =~ /^\/\S/ ||
+				&error($text{'mod_ssl_epasssc'});
+			push(@rv, "exec:".$in{"SSLPassPhraseDialog_script_$i"});
+			}
 		}
 	}
+return ( \@rv );
 }
 
