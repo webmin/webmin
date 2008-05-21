04c0e25cbcb91c5d8750227982272687d70b55dd
Jamie Cameron <jcameron@webmin.com>
2008-05-21 18:07:55 +0000

    Limit process check to a user

--- status/proc-monitor.pl
+++ status/proc-monitor.pl
@@ -7,7 +7,8 @@ local (@found, $count);
 return { 'up' => -1 } if (!&foreign_check("proc"));
 &foreign_require("proc", "proc-lib.pl");
 foreach $p (&foreign_call("proc", "list_processes")) {
-	if ($p->{'args'} =~ /$_[0]->{'cmd'}/i) {
+	if ($p->{'args'} =~ /$_[0]->{'cmd'}/i &&
+	    (!$_[0]->{'asuser'} || $_[0]->{'asuser'} eq $p->{'user'})) {
 		push(@found, $p->{'pid'});
 		$count++;
 		}
@@ -44,6 +45,11 @@ print &ui_table_row($text{'proc_not'},
 
 print &ui_table_row($text{'proc_thresh'},
 	&ui_textbox("thresh", $_[0]->{'thresh'} || 1, 5));
+
+print &ui_table_row($text{'proc_asuser'},
+	&ui_opt_textbox("asuser", $_[0]->{'asuser'}, 13,
+			$text{'proc_anyuser'})." ".
+	&user_chooser_button("asuser"));
 }
 
 sub parse_proc_dialog
@@ -54,6 +60,13 @@ $_[0]->{'cmd'} = $in{'cmd'};
 $_[0]->{'not'} = $in{'not'};
 $in{'thresh'} =~ /^\d+$/ || &error($text{'proc_ethresh'});
 $_[0]->{'thresh'} = $in{'thresh'};
+if ($in{'asuser_def'}) {
+	delete($_[0]->{'asuser'});
+	}
+else {
+	defined(getpwnam($in{'asuser'})) || &error($text{'proc_easuser'});
+	$_[0]->{'asuser'} = $in{'asuser'};
+	}
 }
 
 1;
