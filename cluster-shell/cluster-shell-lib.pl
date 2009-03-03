e43edf5fc6c810b28f2f256587593ed789b53887
Jamie Cameron <jcameron@webmin.com>
2009-03-03 21:29:52 +0000

    Use new module

--- cluster-shell/cluster-shell-lib.pl
+++ cluster-shell/cluster-shell-lib.pl
@@ -1,9 +1,9 @@
 # cluster-shell-lib.pl
 # Doesn't really contain anything ..
 
-do '../web-lib.pl';
+BEGIN { push(@INC, ".."); };
+use WebminCore;
 &init_config();
-do '../ui-lib.pl';
 &foreign_require("servers", "servers-lib.pl");
 
 $commands_file = "$module_config_directory/commands";
