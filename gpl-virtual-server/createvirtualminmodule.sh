55df8a68c3a1dc328aee8132f012a41d8558a4ac
Jamie Cameron <jcameron@webmin.com>
2009-03-23 21:14:27 +0000

    Version bump

--- gpl-virtual-server/createvirtualminmodule.sh
+++ gpl-virtual-server/createvirtualminmodule.sh
@@ -10,7 +10,7 @@ if [ "$?" != "0" ]; then
 	exit 2
 fi
 cd /usr/local/webadmin
-/usr/local/webadmin/create-module.pl --dir virtual-server $2 gpl-virtual-server/$1
+/usr/local/webadmin/create-module.pl --sign --dir virtual-server $2 gpl-virtual-server/$1
 /usr/local/webadmin/showchangelog.pl --html latest gpl-virtual-server >/home/jcameron/webmin.com/vchanges-$1.html
 echo "<a href=vchanges-$1.html>Change log for latest $1 development version of Virtualmin.</a>" >/home/jcameron/webmin.com/vchangelog-latest.html
 echo "<a href=download/virtualmin/virtual-server-$1.wbm.gz>The $1 development version of Virtualmin.</a>" >/home/jcameron/webmin.com/vdevel-latest.html
