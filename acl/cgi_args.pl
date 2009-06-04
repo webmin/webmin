c28b8fd2a013556cec6994e3196b0c8e9d3b2032
Jamie Cameron <jcameron@webmin.com>
2009-06-04 23:18:19 +0000

    Support search links

---
+++ acl/cgi_args.pl
@@ -0,0 +1,27 @@
+
+do 'acl-lib.pl';
+
+sub cgi_args
+{
+my ($cgi) = @_;
+if ($cgi eq 'edit_user.cgi') {
+	local ($u) = grep { &can_edit_user($u->{'name'}) }
+			  &list_users();
+	return $u ? 'user='.&urlize($u->{'name'}) : 'none';
+	}
+elsif ($cgi eq 'edit_group.cgi') {
+	local ($u) = grep { &can_edit_group($u->{'name'}) }
+			  &list_groups();
+	return $u ? 'group='.&urlize($u->{'name'}) : 'none';
+	}
+elsif ($cgi eq 'edit_acl.cgi') {
+	local ($u) = grep { &can_edit_user($u->{'name'}) }
+			  &list_users();
+	if ($u && @{$u->{'modules'}}) {
+		return 'user='.&urlize($u->{'name'}).
+		       '&mod='.$u->{'modules'}->[0];
+		}
+	return 'none';
+	}
+return undef;
+}
