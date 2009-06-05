2a0a7c4871d75597d4846909c94907189f59375b
Jamie Cameron <jcameron@webmin.com>
2009-06-05 20:34:00 +0000

    Added cgi args parser

---
+++ exports/cgi_args.pl
@@ -0,0 +1,12 @@
+
+do 'exports-lib.pl';
+
+sub cgi_args
+{
+my ($cgi) = @_;
+if ($cgi eq 'edit_export.cgi') {
+	my @exps = &list_exports();
+	return @exps ? 'idx='.$exps[0]->{'index'} : 'new=1';
+	}
+return undef;
+}
