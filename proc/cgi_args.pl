2a9900c7dfd43f246133dfc0433e5784bfc95982
Jamie Cameron <jcameron@webmin.com>
2009-06-05 20:45:23 +0000

    Added cgi args parser

---
+++ proc/cgi_args.pl
@@ -0,0 +1,14 @@
+
+do 'proc-lib.pl';
+
+sub cgi_args
+{
+my ($cgi) = @_;
+if ($cgi eq 'edit_proc.cgi') {
+	return '1';	# First process
+	}
+elsif ($cgi eq 'open_files.cgi' || $cgi eq 'trace.cgi') {
+	return 'pid=1';
+	}
+return undef;
+}
