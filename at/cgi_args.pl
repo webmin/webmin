5ea64633fd764654caf265f46361786c1735e663
Jamie Cameron <jcameron@webmin.com>
2009-06-09 00:29:50 +0000

    Add CGI args parser

---
+++ at/cgi_args.pl
@@ -0,0 +1,13 @@
+
+do 'at-lib.pl';
+
+sub cgi_args
+{
+my ($cgi) = @_;
+if ($cgi eq 'edit_job.cgi') {
+	my @jobs = &list_atjobs();
+	@jobs = grep { &can_edit_user(\%access, $_->{'user'}) } @jobs;
+	return @jobs ? 'id='.$jobs[0]->{'idx'} : 'none';
+	}
+return undef;
+}
