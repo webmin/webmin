b63fe2d3591dbd7cae3d8ba034cf261800789be9
Jamie Cameron <jcameron@webmin.com>
2009-06-09 00:52:36 +0000

    Add CGI args parser

---
+++ software/cgi_args.pl
@@ -0,0 +1,25 @@
+
+do 'software-lib.pl';
+
+sub cgi_args
+{
+my ($cgi) = @_;
+if ($cgi eq 'search.cgi') {
+	# Example search
+	return 'search=net';
+	}
+elsif ($cgi eq 'edit_pack.cgi' || $cgi eq 'list_pack.cgi') {
+	# Package for /bin/ls
+	local %file;
+	&installed_file('/bin/ls');
+	my @pkgs = split(/\s+/, $file{'packages'});
+	my @vers = split(/\s+/, $file{'versions'});
+	return 'package='.&urlize($pkgs[0]).
+	       '&version='.&urlize($vers[0]);
+	}
+elsif ($cgi eq 'file_info.cgi') {
+	# Info on file /bin/ls
+	return 'file=/bin/ls';
+	}
+return undef;
+}
