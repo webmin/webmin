7c2c009b3f69d77e466ac8ae36b4811a97a11874
Jamie Cameron <jcameron@webmin.com>
2009-10-07 23:55:35 -0700

    Remove old Xinha version

--- mailboxes/xinha/modules/FullScreen/lang/fr.js
+++
@@ -1,5 +0,0 @@
-// I18N constants
-// LANG: "fr", ENCODING: UTF-8
-{
-  "Maximize/Minimize Editor": "Agrandir/Réduire l'éditeur"
-};
--- mailboxes/xinha/modules/FullScreen/lang/nb.js
+++
@@ -1,6 +0,0 @@
-// I18N constants
-// LANG: "nb", ENCODING: UTF-8
-// translated: Kim Steinhaug, http://www.steinhaug.com/, kim@steinhaug.com
-{
-  "Maximize/Minimize Editor": "Maksimer/Minimer WYSIWYG vindu"
-};
