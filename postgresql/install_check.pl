4f8dae5d545c44f137422684a35520f2e1b6757a
Jamie Cameron <jcameron@webmin.com>
2009-11-27 12:34:31 -0800

    Don't complain about hba.conf is postgresql is remote

--- postgresql/install_check.pl
+++ postgresql/install_check.pl
@@ -11,7 +11,7 @@ sub is_installed
 return 0 if (!-x $config{'psql'});
 if ($_[0]) {
 	# Check for .conf and if can login
-	return 1 if (!-r $hba_conf_file);
+	return 1 if (!-r $hba_conf_file && &is_postgresql_local());
 	return 2 if (&is_postgresql_running() == 1);
 	}
 return 1;
