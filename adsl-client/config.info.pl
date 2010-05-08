7c06a12fefd31b01448c720d7e502f90c29a01ab
Jamie Cameron <jcameron@webmin.com>
2010-05-08 15:44:30 -0700

    Polish update

---
+++ adsl-client/config.info.pl
@@ -0,0 +1,8 @@
+line2=Konfiguracja systemu,11
+pppoe_conf=Plik konfiguracji PPPOE ADSL,0
+conf_style=Format pliku konfiguracyjnego,1,0-Standardowa konfiguracja RP-PPPoE,1-Plik sieciowy Redhat
+pap_file=Plik kont PPP,0
+pppoe_cmd=Pe³na ¶cie¿ka do komendy pppoe,0
+start_cmd=Komenda do podniesienia po³±czenia ADSL,0
+stop_cmd=Komenda do zamkniêcia po³±czenia ADSL,0
+status_cmd=Komenda do pobrania stanu po³±czenia ADSL,0
