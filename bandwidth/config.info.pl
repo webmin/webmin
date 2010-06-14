af75be7645cc1d34105c8086c1e9e82002f6e831
Jamie Cameron <jcameron@webmin.com>
2010-06-14 11:54:08 -0700

    Polish update

---
+++ bandwidth/config.info.pl
@@ -0,0 +1,3 @@
+firewall_system=Rodzaj firewalla,4,firewall-IPtables,,ipfw-IPFW,ipfilter-IPFilter,shorewall-Shorewall,-Wykryj automatycznie
+bandwidth_log=Plik logu do utworzenia dla komunikatów firewalla,0
+bandwidth_dir=Katalog dla danych pasma,3,Domy¶lny (/etc/webmin/bandwidth/hours)
