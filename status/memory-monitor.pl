3d828b2c4c66ec127cad250347a900cdeb990650
Jamie Cameron <jcameron@webmin.com>
2010-11-16 12:00:43 -0800

    Ability to also check free swap space https://sourceforge.net/tracker/index.php?func=detail&aid=3110202&group_id=17457&atid=117457#

--- status/memory-monitor.pl
+++ status/memory-monitor.pl
@@ -11,11 +11,21 @@ if ($@) {
 	return { 'up' => -1 };
 	}
 elsif ($mem[1] < $_[0]->{'min'}) {
-	return { 'up' => 0 };
+	return { 'up' => 0,
+		 'desc' => &text('memory_freelow', &nice_size($mem[1]*1024)) };
+	}
+elsif ($mem[2] && $mem[3] < $_[0]->{'minswap'}) {
+	return { 'up' => 0,
+		 'desc' => &text('memory_freelowswap',
+				 &nice_size($mem[3]*1024)) };
 	}
 else {
+	my @desc = ( &text('memory_free2', &nice_size($mem[1]*1024)) );
+	if ($mem[2]) {
+		push(@desc, &text('memory_freeswap', &nice_size($mem[2]*1024)));
+		}
 	return { 'up' => 1,
-		 'desc' => &text('memory_free2', &nice_size($mem[1]*1024)) };
+		 'desc' => join(", ", @desc) };
 	}
 }
 
@@ -23,6 +33,9 @@ sub show_memory_dialog
 {
 print &ui_table_row($text{'memory_min2'},
 	&ui_bytesbox("min", $_[0]->{'min'}*1024));
+
+print &ui_table_row($text{'memory_minswap'},
+	&ui_bytesbox("minswap", $_[0]->{'minswap'}*1024));
 }
 
 sub parse_memory_dialog
@@ -32,5 +45,7 @@ sub parse_memory_dialog
 defined(&proc::get_memory_info) || &error($text{'memory_eproc'});
 $in{'min'} =~ /^[0-9\.]+$/ || &error($text{'memory_emin'});
 $_[0]->{'min'} = $in{'min'}*$in{'min_units'}/1024;
+$in{'minswap'} =~ /^[0-9\.]+$/ || &error($text{'memory_eminswap'});
+$_[0]->{'minswap'} = $in{'minswap'}*$in{'minswap_units'}/1024;
 }
 
