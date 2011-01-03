5dbdcd1e27884bf97221d6c4ae0fccf3aba4d785
Jamie Cameron <jcameron@webmin.com>
2011-01-03 11:09:52 -0800

    Support for MySQL .cnf file backup

---
+++ mysql/backup_config.pl
@@ -0,0 +1,44 @@
+
+do 'mysql-lib.pl';
+
+# backup_config_files()
+# Returns files and directories that can be backed up
+sub backup_config_files
+{
+return ( $config{'my_cnf'} );
+}
+
+# pre_backup(&files)
+# Called before the files are actually read
+sub pre_backup
+{
+return undef;
+}
+
+# post_backup(&files)
+# Called after the files are actually read
+sub post_backup
+{
+return undef;
+}
+
+# pre_restore(&files)
+# Called before the files are restored from a backup
+sub pre_restore
+{
+return undef;
+}
+
+# post_restore(&files)
+# Called after the files are restored from a backup
+sub post_restore
+{
+if (&is_mysql_running()) {
+	&stop_mysql();
+	return &start_mysql();
+	}
+return undef;
+}
+
+1;
+
