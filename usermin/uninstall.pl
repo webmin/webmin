67e93b1f44f2708c253d32418bcbf89d99739001
Jamie Cameron <jcameron@webmin.com>
2011-01-19 10:24:00 -0800

    Make usermin.pl cron more aggressive

--- usermin/uninstall.pl
+++ usermin/uninstall.pl
@@ -5,14 +5,12 @@ require 'usermin-lib.pl';
 
 sub module_uninstall
 {
-if ($config{'update'}) {
-	&foreign_require("cron", "cron-lib.pl");
-	$cron_cmd = "$module_config_directory/update.pl";
-	foreach $j (&cron::list_cron_jobs()) {
-		if ($j->{'user'} eq 'root' &&
-		    $j->{'command'} eq $cron_cmd) {
-			&cron::delete_cron_job($j);
-			}
+&foreign_require("cron", "cron-lib.pl");
+my $cron_cmd = "$module_config_directory/update.pl";
+foreach my $j (&cron::list_cron_jobs()) {
+	if ($j->{'user'} eq 'root' &&
+	    $j->{'command'} eq $cron_cmd) {
+		&cron::delete_cron_job($j);
 		}
 	}
 }
