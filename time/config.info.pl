a9f2637f4b27549d0ce5427d88d39d8b266f151e
Jamie Cameron <jcameron@webmin.com>
2011-05-11 11:29:44 -0700

    Polish update

--- time/config.info.pl
+++ time/config.info.pl
@@ -1,7 +1,9 @@
+line1=Konfigurowalne opcje,11
 lease=Dozwolona liczba sekund ró¿nicy pomiêdzy czasem systemowym i&nbsp;sprzêtowym,0
 timeserver=Domy¶lny serwer czasu,3,Brak
+ntp_only=U¿yæ tylko NTP do synchronizacji czasu?,1,1-Tak,0-Nie
+line2=Konfiguracja systemu,11
 hwtime=System obs³uguje czas sprzêtowy,1,1-Tak,0-Nie
 seconds=Format dla ustawiania czasu systemowego,1,1-MMDDGGMMRRRR.SS,0-MMDDGGMMRR,2-RRRRMMDDGGMM.SS
-zonelink=Plik opisu strefy czasowej,0
-zonetab=Plik z&nbsp;list± stref czasowych,0
-zonedir=Katalog z&nbsp;plikami opisu stref czasowych,0
+zone_style=Metoda konfiguracji strefy czasowej,4,linux-Linux,freebsd-FreeBSD,solaris-Solaris,-&lt;Nie obs³ugiwany na tym systemie operacyjnym&gt;
+hwclock_flags=Flagi linii poleceñ dla hwclock,10,-Brak,sysconfig-Z /etc/sysconfig/clock
