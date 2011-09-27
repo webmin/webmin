fff339f501157628b53b5deed8dfc3493ddd92e0
Jamie Cameron <jcameron@webmin.com>
2011-09-27 11:56:16 -0700

    Polish updates

---
+++ webmin/config.info.pl
@@ -0,0 +1,4 @@
+standard_url=URL listy modu³ów standardowych,3,www.webmin.com
+third_url=URL listy modu³ów osób trzecich,3,www.webmin.com
+cron_mode=Pokazuj czasy aktualizacji jako,1,0-Prosty,1-Ustawienia czasu Crona
+warn_days=Na ile dni przed wyga¶niêciem has³a ostrzegaæ u¿ytkowników?,0,5
