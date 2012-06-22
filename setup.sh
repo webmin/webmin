bf7fe5f44adcae86f0d50f9c68b7d65306f38325
Jamie Cameron <jcameron@webmin.com>
2012-06-22 23:47:51 +0800

    Increase default key size to 2048 bits https://virtualmin.com/node/22562

--- setup.sh
+++ setup.sh
@@ -533,7 +533,7 @@ else
 	if [ "$?" = "0" ]; then
 		# We can generate a new SSL key for this host
 		host=`hostname`
-		openssl req -newkey rsa:512 -x509 -nodes -out $tempdir/cert -keyout $tempdir/key -days 1825 >/dev/null 2>&1 <<EOF
+		openssl req -newkey rsa:2048 -x509 -nodes -out $tempdir/cert -keyout $tempdir/key -days 1825 >/dev/null 2>&1 <<EOF
 .
 .
 .
