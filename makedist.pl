22f21ca2ac880c7faa8c98c5f966451586650ee6
Jamie Cameron <jcameron@webmin.com>
2012-09-08 09:46:41 -0700

    INclude new module in builds

--- makedist.pl
+++ makedist.pl
@@ -72,7 +72,7 @@ else {
 	  "mailcap", "blue-theme", "ldap-client", "phpini", "filter",
 	  "bacula-backup", "ldap-server", "exim", "tcpwrappers",
 	  "package-updates", "system-status", "webmincron", "ajaxterm",
-	  "shorewall6",
+	  "shorewall6", "iscsi-server",
 	  );
 	}
 @dirlist = ( "Webmin" );
