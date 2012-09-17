ebda4b84ab5ba6e61c332238cf7b2aa8beffbcad
Jamie Cameron <jcameron@webmin.com>
2012-09-16 22:19:19 -0700

    Config backup definition script

---
+++ iscsi-client/backup_config.pl
@@ -0,0 +1,41 @@
+
+do 'iscsi-client-lib.pl';
+
+# backup_config_files()
+# Returns files and directories that can be backed up
+sub backup_config_files
+{
+return ( $config{'config_file'}, $config{'initiator_file'},
+	 $config{'ifaces_dir'} );
+}
+
+# pre_backup(&files)
+# Called before the files are actually read
+sub pre_backup
+{
+return undef;
+}
+
+# post_backup(&files)
+# Called after the files are actually read
+sub post_backup
+{
+return undef;
+}
+
+# pre_restore(&files)
+# Called before the files are restored from a backup
+sub pre_restore
+{
+return undef;
+}
+
+# post_restore(&files)
+# Called after the files are restored from a backup
+sub post_restore
+{
+return undef;
+}
+
+1;
+
