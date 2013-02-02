62779a7f97a8da265e1d6449ce04641664882290
Jamie Cameron <jcameron@webmin.com>
2013-02-01 21:53:30 -0800

    Polish updates from Piotr Kozica

---
+++ usermin/config.info.pl
@@ -0,0 +1,4 @@
+usermin_dir=Katalog konfiguracyjny Usermina,0
+cron_mode=Poka¿ czas aktualizacji jako,1,0-Prosty interfejs,1-Cron time selector
+host=Nazwa hosta Usermina do prze³±czania u¿ytkowników,3,Taki sam jak w Webmin
+port=Port Usermina do prze³±czania u¿ytkowników,3,Okre¶lony automatycznie
