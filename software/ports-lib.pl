c346d6e572b50e434588f8b7e3a073c522d24721
Jamie Cameron <jcameron@webmin.com>
2013-03-19 21:40:05 -0700

    use samba 3 packages

--- software/ports-lib.pl
+++ software/ports-lib.pl
@@ -111,7 +111,7 @@ return $name eq "apache" ? "apache22 ap22-mod_.*" :
        $name eq "openssh" ? "openssh-portable" :
        $name eq "postgresql" ? "postgresql-server" :
        $name eq "openldap" ? "openldap-server openldap-client" :
-       $name eq "samba" ? "samba36 samba36-nmblookup samba36-smbclient" :
+       $name eq "samba" ? "samba36 samba36-smbclient samba36-nmblookup" :
        			  $name;
 }
 
