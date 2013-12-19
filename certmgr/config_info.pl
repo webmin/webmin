d7fbd5b0936439ceb78c4db1aceac40c40a0f866
Nawawi Jamili <nawawi@rutweb.com>
2013-12-19 16:03:42 +0800

    remove unused function

--- certmgr/config_info.pl
+++ certmgr/config_info.pl
@@ -2,14 +2,4 @@
 # do 'certmgr-lib.pl';
 require 'certmgr-lib.pl';
 
-sub show_cfile
-{
-local ($value) = @_;
-return "<input name=cfile size=30 value='$value'> ". &file_chooser_button("cfile")." "."<a href='/certmgr/edit_file.cgi?file=$value'>Edit..</a>";
-}
-
-sub parse_cfile
-{
-return $in{'cfile'};
-}
 
