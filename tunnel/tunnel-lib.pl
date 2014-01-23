a4bf4d5d3b8b90621ecc28ff75f4e7d600837a88
Jamie Cameron <jcameron@webmin.com>
2014-01-23 09:53:20 -0800

    Fix modification of host variable https://sourceforge.net/p/webadmin/bugs/4358/

--- tunnel/tunnel-lib.pl
+++ tunnel/tunnel-lib.pl
@@ -8,13 +8,13 @@ use WebminCore;
 sub fix_end_url {
     my($url) = @_;
     if ( $url =~ m/^(http|https):\/\/(\S+)$/ ) {
-        $schema = $1."://";
-        $host = $2;
+        my $schema = $1."://";
+        my $host = $2;
 
         # check: http://aa.com
         # check: http://aa.com/bb.html
         # check: http://aa.com/bb/cc.html
-        $host_test = $2;
+        my $host_test = $2;
         $host_test =~ s/\/?[^\/]*\/*$//;
         if ( $host_test eq '' ) {
             $url = "$schema$host/";
