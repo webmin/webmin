ef337235114f41fd55940168e1f2a9c940e3f9d8
Jamie Cameron <jcameron@webmin.com>
2014-02-14 13:33:57 -0800

    use ui-lib function

--- shell/acl_security.pl
+++ shell/acl_security.pl
@@ -5,13 +5,9 @@ do 'shell-lib.pl';
 # Output HTML for editing security options for the shell module
 sub acl_security_form
 {
-print "<tr> <td><b>$text{'acl_user'}</b></td> <td colspan=3>\n";
-printf "<input type=radio name=user_def value=1 %s> %s\n",
-	$_[0]->{'user'} ? '' : 'checked', $text{'acl_user_def'};
-printf "<input type=radio name=user_def value=0 %s>\n",
-	$_[0]->{'user'} ? 'checked' : '';
-print "<input name=user size=8 value='$_[0]->{'user'}'> ",
-      &user_chooser_button("user"),"</td> </tr>\n";
+print &ui_table_row($text{'acl_user'},
+	&ui_opt_textbox("user", $_[0]->{'user'}, 20, $text{'acl_user_def'})." ".
+	&user_chooser_button("user"));
 }
 
 # acl_security_save(&options)
