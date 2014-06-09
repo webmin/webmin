097912afc4f821c938953018a9f306931e0cf6f6
Jamie Cameron <jcameron@webmin.com>
2014-06-08 22:15:07 -0700

    Fix syntax error

--- sendmail/aliases-lib.pl
+++ sendmail/aliases-lib.pl
@@ -148,13 +148,13 @@ for($i=0; $i<=@values; $i++) {
 	local $valtxt = &ui_textbox("val_$i", $val, 30);
 	local $edlnk;
 	if ($type == 2 && $a) {
-		$edlnk = &ui_link("edit_afile.cgi?file=$val&num=$a->{'num'}",$text{'aform_afile'})\n";
+		$edlnk = &ui_link("edit_afile.cgi?file=$val&num=$a->{'num'}",$text{'aform_afile'});
 		}
 	elsif ($type == 5 && $a) {
-		$edlnk = &ui_link("edit_rfile.cgi?file=$val&num=$a->{'num'}",$text{'aform_afile'})\n";
+		$edlnk = &ui_link("edit_rfile.cgi?file=$val&num=$a->{'num'}",$text{'aform_afile'});
 		}
 	elsif ($type == 6 && $a) {
-		$edlnk = &ui_link("edit_ffile.cgi?file=$val&num=$a->{'num'}",$text{'aform_afile'})\n";
+		$edlnk = &ui_link("edit_ffile.cgi?file=$val&num=$a->{'num'}",$text{'aform_afile'});
 		}
 	print &ui_table_row(&hlink($text{'aform_val'},"alias_to"),
 			      $typesel."\n".$valtxt."\n".$edlnk);
