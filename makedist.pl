8ec0c144d24d27941200f430be4320de9f38ee03
Jamie Cameron <jcameron@webmin.com>
2015-06-09 22:32:37 -0700

    Prepare module for inclusion in standard distribution

--- makedist.pl
+++ makedist.pl
@@ -75,7 +75,7 @@ else {
 	  "package-updates", "system-status", "webmincron", "ajaxterm",
 	  "shorewall6", "iscsi-server", "iscsi-client", "gray-theme",
 	  "iscsi-target", "iscsi-tgtd", "bsdfdisk", "fail2ban",
-	  "authentic-theme",
+	  "authentic-theme", "firewalld",
 	  );
 	}
 @dirlist = ( "Webmin" );
