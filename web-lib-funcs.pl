5db4f9568785073941d51e05d0ffab6b3b244063
Jamie Cameron <jcameron@webmin.com>
2015-08-02 13:00:45 -0700

    Xinha requires use of eval

--- web-lib-funcs.pl
+++ web-lib-funcs.pl
@@ -766,7 +766,7 @@ if (!$gconfig{'no_frame_options'}) {
 	print "X-Frame-Options: SAMEORIGIN\n";
 	}
 if (!$gconfig{'no_content_security_policy'}) {
-	print "Content-Security-Policy: script-src 'self' 'unsafe-inline'; frame-src 'self'; child-src 'self'\n";
+	print "Content-Security-Policy: script-src 'self' 'unsafe-inline' 'unsafe-eval'; frame-src 'self'; child-src 'self'\n";
 	}
 if (defined($_[0])) {
 	print "Content-type: text/html; Charset=$_[0]\n\n";
