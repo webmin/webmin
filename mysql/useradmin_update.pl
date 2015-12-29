bbbdb849ebd13dd4d1b9d397b7bcb03d0f318822
Jamie Cameron <jcameron@webmin.com>
2015-12-28 17:38:00 -0800

    fix user sync code to handle new password columns

--- mysql/useradmin_update.pl
+++ mysql/useradmin_update.pl
@@ -7,23 +7,25 @@ do 'mysql-lib.pl';
 # Create a new mysql user if syncing is enabled
 sub useradmin_create_user
 {
+my ($user) = @_;
 if ($config{'sync_create'}) {
-	local %privs;
-	map { $privs{$_}++ } split(/\s+/, $config{'sync_privs'});
-	local @yesno;
-	for($i=3; $i<=&user_priv_cols()+3-1; $i++) {
-		push(@yesno, $privs{$i} ? "'Y'" : "'N'");
-		}
-	local @desc = &table_structure($master_db, 'user');
-	local $sql = sprintf "insert into user (%s) values ('%s', '%s', %s, %s)",
-		join(",", map { $desc[$_]->{'field'} } (0 .. &user_priv_cols()+3-1)),
+	my %privs = map { $_, 1 } split(/\s+/, $config{'sync_privs'});
+        my @pfields = map { $_->[0] } &priv_fields('user');
+	my $sql = "insert into user (host, user, ".
+		  join(", ", @pfields).") values (?, ?, ".
+		  join(", ", map { "?" } @pfields).")";
+	&execute_sql_logged($master_db, $sql,
 		$config{'sync_host'},
-		$_[0]->{'user'},
-		$_[0]->{'passmode'} == 3 ? "$password_func('$_[0]->{'plainpass'}')" :
-		$_[0]->{'passmode'} == 0 ? "''" : "'*'",
-		join(",", @yesno);
-	&execute_sql_logged($master_db, $sql);
+		$user->{'user'},
+		(map { $privs{$_} ? 'Y' : 'N' } @pfields));
 	&execute_sql_logged($master_db, 'flush privileges');
+	if ($user->{'passmode'} == 3) {
+		$esc = &escapestr($user->{'plainpass'});
+		&execute_sql_logged($master_db,
+			"set password for '".$user->{'user'}."'\@'".
+			$config{'sync_host'}."' = ".
+			"$password_func('$esc')");
+		}
 	}
 }
 
@@ -31,15 +33,16 @@ if ($config{'sync_create'}) {
 # Delete a mysql user
 sub useradmin_delete_user
 {
+my ($user) = @_;
 if ($config{'sync_delete'}) {
 	&execute_sql_logged($master_db,
-		    "delete from user where user = '$_[0]->{'user'}'");
+		"delete from user where user = ?", $user->{'user'});
 	&execute_sql_logged($master_db,
-		    "delete from db where user = '$_[0]->{'user'}'");
+		"delete from db where user = ?", $user->{'user'});
 	&execute_sql_logged($master_db,
-		    "delete from tables_priv where user = '$_[0]->{'user'}'");
+		"delete from tables_priv where user = ?", $user->{'user'});
 	&execute_sql_logged($master_db,
-		    "delete from columns_priv where user = '$_[0]->{'user'}'");
+		"delete from columns_priv where user = ?", $user->{'user'});
 	&execute_sql_logged($master_db, 'flush privileges');
 	}
 }
@@ -48,38 +51,36 @@ if ($config{'sync_delete'}) {
 # Update a mysql user
 sub useradmin_modify_user
 {
+my ($user) = @_;
 if ($config{'sync_modify'}) {
-	local $sql;
-	$_[0]->{'olduser'} ||= $_[0]->{'user'};	# In case not changed
-	if ($_[0]->{'passmode'} == 4) {
-		# Not changing password
-		$sql = sprintf "update user set user = '%s' where user = '%s'", $_[0]->{'user'}, $_[0]->{'olduser'};
-		}
-	elsif ($_[0]->{'passmode'} == 3) {
-		# Setting new password
-		$sql = sprintf "update user set user = '%s', password = $password_func('%s') where user = '%s'", $_[0]->{'user'}, $_[0]->{'plainpass'}, $_[0]->{'olduser'};
-		}
-	elsif ($_[0]->{'passmode'} == 0) {
-		# No password
-		$sql = sprintf "update user set user = '%s', password = '' where user = '%s'", $_[0]->{'user'}, $_[0]->{'olduser'};
-		}
-	else {
-		# Assume locked
-		$sql = sprintf "update user set user = '%s', password = '*' where user = '%s'", $_[0]->{'user'}, $_[0]->{'olduser'};
-		}
-	&execute_sql_logged($master_db, $sql);
-	if ($_[0]->{'user'} ne $_[0]->{'olduser'}) {
+	$user->{'olduser'} ||= $user->{'user'};	# In case not changed
+
+	# Rename user if needed
+	if ($user->{'user'} ne $user->{'olduser'}) {
 		&execute_sql_logged($master_db,
-			"update db set user = '$_[0]->{'user'}' ".
-			"where user = '$_[0]->{'olduser'}'");
+			"update db set user = '$user->{'user'}' ".
+			"where user = '$user->{'olduser'}'");
 		&execute_sql_logged($master_db,
-			"update tables_priv set user = '$_[0]->{'user'}' ".
-			"where user = '$_[0]->{'olduser'}'");
+			"update tables_priv set user = '$user->{'user'}' ".
+			"where user = '$user->{'olduser'}'");
 		&execute_sql_logged($master_db,
-			"update columns_priv set user = '$_[0]->{'user'}' ".
-			"where user = '$_[0]->{'olduser'}'");
+			"update columns_priv set user = '$user->{'user'}' ".
+			"where user = '$user->{'olduser'}'");
 		}
 	&execute_sql_logged($master_db, 'flush privileges');
+
+	# Update password if changed
+	if ($user->{'passmode'} == 3) {
+		my $d = &execute_sql_safe($master_db,
+		    "select host from user where user = ?", $user->{'user'});
+		my @hosts = map { $_->[0] } @{$d->{'data'}};
+		my $esc = &escapestr($user->{'plainpass'});
+		foreach my $host (@hosts) {
+			$sql = "set password for '".$user->{'user'}.
+			       "'\@'".$host."' = $password_func('$esc')";
+			&execute_sql_logged($master_db, $sql);
+			}
+		}
 	}
 }
 
