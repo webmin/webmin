f2374b1fa20f680313a9827de41abbccb0478fb0
Jamie Cameron <jcameron@webmin.com>
2016-03-28 17:12:09 -0700

    ui-lib conversion

--- change-user/acl_security.pl
+++ change-user/acl_security.pl
@@ -8,31 +8,24 @@ our (%text, %in);
 # Output HTML for editing security options for the acl module
 sub acl_security_form
 {
-print "<tr> <td><b>$text{'acl_lang'}</b></td> <td nowrap>\n";
-printf "<input type=radio name=lang value=1 %s> $text{'yes'}\n",
-	$_[0]->{'lang'} ? 'checked' : '';
-printf "<input type=radio name=lang value=0 %s> $text{'no'}</td>\n",
-	$_[0]->{'lang'} ? '' : 'checked';
+my ($o) = @_;
+print &ui_table_row($text{'acl_lang'},
+	&ui_yesno_radio("lang", $o->{'lang'}));
 
-print "<td><b>$text{'acl_theme'}</b></td> <td nowrap>\n";
-printf "<input type=radio name=theme value=1 %s> $text{'yes'}\n",
-	$_[0]->{'theme'} ? 'checked' : '';
-printf "<input type=radio name=theme value=0 %s> $text{'no'}</td> </tr>\n",
-	$_[0]->{'theme'} ? '' : 'checked';
+print &ui_table_row($text{'acl_theme'},
+	&ui_yesno_radio("theme", $o->{'theme'}));
 
-print "<tr> <td><b>$text{'acl_pass'}</b></td> <td nowrap>\n";
-printf "<input type=radio name=pass value=1 %s> $text{'yes'}\n",
-	$_[0]->{'pass'} ? 'checked' : '';
-printf "<input type=radio name=pass value=0 %s> $text{'no'}</td> </tr>\n",
-	$_[0]->{'pass'} ? '' : 'checked';
+print &ui_table_row($text{'acl_pass'},
+	&ui_yesno_radio("pass", $o->{'pass'}));
 }
 
 # acl_security_save(&options)
 # Parse the form for security options for the acl module
 sub acl_security_save
 {
-$_[0]->{'lang'} = $in{'lang'};
-$_[0]->{'theme'} = $in{'theme'};
-$_[0]->{'pass'} = $in{'pass'};
+my ($o) = @_;
+$o->{'lang'} = $in{'lang'};
+$o->{'theme'} = $in{'theme'};
+$o->{'pass'} = $in{'pass'};
 }
 
