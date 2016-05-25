f32aa173173ee7646c4264e513513fda499fbd9d
Jamie Cameron <jamiecameron@jamiecameron-macbookpro2.roam.corp.google.com>
2016-05-26 00:02:59 +0200

    More perl strict enforcement

--- time/acl_security.pl
+++ time/acl_security.pl
@@ -1,28 +1,27 @@
 
+use strict;
+use warnings;
 require 'time-lib.pl';
+our (%text);
 
 sub acl_security_form
 {
-    print(
-	"<tr>",
-	    "<td><b>", $text{ 'acl_sys' }, "</b></td>",
-	    "<td><input type=radio name=sysdate value=0 ", $_[0] -> { 'sysdate' } == 0 ? "checked" : "", ">", $text{ 'acl_yes' }, " <input type=radio name=sysdate value=1 ", $_[0] -> { 'sysdate' } == 1 ? "checked" : "", ">", $text{ 'acl_no' },
-	"</tr><tr>",
-	    "<td><b>", $text{ 'acl_hw' }, "</b></td>",
-	    "<td><input type=radio name=hwdate value=0 ", $_[0] -> { 'hwdate' } == 0 ? "checked" : "", ">", $text{ 'acl_yes' }, " <input type=radio name=hwdate value=1 ", $_[0] -> { 'hwdate' } == 1 ? "checked" : "", ">", $text{ 'acl_no' },
-	"</tr><tr>",
-	    "<td><b>", $text{ 'acl_timezone' }, "</b></td>",
-	    "<td><input type=radio name=timezone value=1 ", $_[0] -> { 'timezone' } == 1 ? "checked" : "", ">", $text{ 'acl_yes' }, " <input type=radio name=timezone value=0 ", $_[0] -> { 'timezone' } == 0 ? "checked" : "", ">", $text{ 'acl_no' },
-	"</tr><tr>",
-	    "<td><b>", $text{ 'acl_ntp' }, "</b></td>",
-	    "<td><input type=radio name=ntp value=1 ", $_[0] -> { 'ntp' } == 1 ? "checked" : "", ">", $text{ 'acl_yes' }, " <input type=radio name=ntp value=0 ", $_[0] -> { 'ntp' } == 0 ? "checked" : "", ">", $text{ 'acl_no' },
-	"</tr>\n");
+my ($o) = @_;
+print &ui_table_row(text{'acl_sys'},
+	&ui_yesno_radio("sysdate", $o->{'sysdate'}, 0, 1));
+print &ui_table_row(text{'acl_hw'},
+	&ui_yesno_radio("hwdate", $o->{'hwdate'}, 0, 1));
+print &ui_table_row(text{'acl_timezone'},
+	&ui_yesno_radio("timezone", $o->{'timezone'}));
+print &ui_table_row(text{'acl_ntp'},
+	&ui_yesno_radio("ntp", $o->{'ntp'}));
 }
 
 sub acl_security_save
 {
-    $_[0] -> { 'sysdate' } = $in{ 'sysdate' };
-    $_[0] -> { 'hwdate' } = $in{ 'hwdate' };
-    $_[0] -> { 'timezone' } = $in{ 'timezone' };
-    $_[0] -> { 'ntp' } = $in{ 'ntp' };
+my ($o, $in) = @_;
+$o->{'sysdate'} = $in->{'sysdate'};
+$o->{'hwdate'} = $in->{'hwdate'};
+$o->{'timezone'} = $in->{'timezone'};
+$o->{'ntp'} = $in->{'ntp'};
 }
--- time/log_parser.pl
+++ time/log_parser.pl
@@ -1,20 +1,23 @@
 # log_parser.pl
 # Functions for parsing this module's logs
 
+use strict;
+use warnings;
 do 'time-lib.pl';
+our (%text);
 
 # parse_webmin_log(user, script, action, type, object, &params)
 # Converts logged information from this module into human-readable form
 sub parse_webmin_log
 {
-local ($user, $script, $action, $type, $object, $p, $long) = @_;
+my ($user, $script, $action, $type, $object, $p, $long) = @_;
 if ($action eq 'remote') {
-	local $tm = localtime($object);
+	my $tm = localtime($object);
 	return &text($long ? "log_remote_${type}_l" : "log_remote_${type}",
 	     "<i>$tm</i>", "<tt>".&html_escape($p->{'timeserver'})."</tt>");
 	}
 elsif ($action eq 'set') {
-	local $tm = localtime($object);
+	my $tm = localtime($object);
 	return &text("log_set_${type}", "<i>$tm</i>");
 	}
 elsif ($action eq 'sync') {
