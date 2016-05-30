7ac845a01bed4ebaeeabf0cedc4dbf6042bcf2de
Jamie Cameron <jcameron@webmin.com>
2016-05-30 15:09:23 -0700

    remove debug messages https://sourceforge.net/p/webadmin/bugs/4768/

---
+++ record-failed.pl
@@ -0,0 +1,15 @@
+#!/usr/local/bin/perl
+# Write an actions log for a login
+
+BEGIN { push(@INC, ".."); };
+use WebminCore;
+
+&init_config();
+($username, $sid, $remoteip, $localip) = @ARGV;
+if ($username && $sid && $remoteip) {
+	$WebminCore::remote_user = $remote_user = $username;
+	$main::session_id = $sid;
+	$0 = "miniserv.pl";
+	&webmin_log("login", undef, undef, undef, "global", undef,
+		    undef, $remoteip);
+	}
