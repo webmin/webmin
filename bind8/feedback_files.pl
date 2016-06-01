79f1c9368fe7c573b029916d2108e6dc21e14ad4
Joe Cooper <joe@virtualmin.com>
2016-06-01 03:02:04 -0700

    strict/warn

--- bind8/feedback_files.pl
+++ bind8/feedback_files.pl
@@ -1,20 +1,23 @@
+use strict;
+use warnings;
+our (%config);
 
 do 'bind8-lib.pl';
 
 sub feedback_files
 {
-local @rv = ( &make_chroot($config{'named_conf'}) );
-local $conf = &get_config();
-local @views = &find("view", $conf);
-local @zones;
-foreach $v (@views) {
+my @rv = ( &make_chroot($config{'named_conf'}) );
+my $conf = &get_config();
+my @views = &find("view", $conf);
+my @zones;
+foreach my $v (@views) {
 	push(@zones, &find("zone", $v->{'members'}));
 	}
 push(@zones, &find("zone", $conf));
-foreach $z (@zones) {
-	local $tv = &find("type", $z->{'members'});
+foreach my $z (@zones) {
+	my $tv = &find("type", $z->{'members'});
 	if ($tv->{'value'} eq 'primary') {
-		local $fv = &find("file", $z->{'members'});
+		my $fv = &find("file", $z->{'members'});
 		if ($fv) {
 			push(@rv, $fv->{'value'});
 			}
