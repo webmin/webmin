ac9a3b7c41aa665ca8b9e3b1443acac3bc78b24c
Joe Cooper <joe@virtualmin.com>
2016-06-17 23:15:07 -0700

    strict/warn

--- at/solaris-lib.pl
+++ at/solaris-lib.pl
@@ -1,49 +1,54 @@
 # solaris-lib.pl
+use strict;
+use warnings;
+our %config;
 
 use POSIX;
 
 sub list_atjobs
 {
-local @rv;
-opendir(DIR, $config{'at_dir'});
-while($f = readdir(DIR)) {
-	local $p = "$config{'at_dir'}/$f";
+my @rv;
+opendir(my $DIR, $config{'at_dir'});
+while(my $f = readdir($DIR)) {
+	my $p = "$config{'at_dir'}/$f";
 	if ($f =~ /^(\d+)\.a$/) {
-		local @st = stat($p);
-		local $job = { 'id' => $f,
+		my @st = stat($p);
+		my $job = { 'id' => $f,
 			       'date' => $1,
 			       'user' => scalar(getpwuid($st[4])),
 			       'created' => $st[9] };
-		open(FILE, $p);
-		while(<FILE>) {
+		open(my $FILE, "<", $p);
+		while(<$FILE>) {
 			$job->{'cmd'} .= $_;
 			}
-		close(FILE);
+		close($FILE);
 		$job->{'realcmd'} = $job->{'cmd'};
 		$job->{'realcmd'} =~ s/^[\000-\177]+#ident.*\ncd\s+\S+\numask\s+\S+\n//;
 		$job->{'realcmd'} =~ s/^ulimit\s+\S+\n//;
 		push(@rv, $job);
 		}
 	}
-closedir(DIR);
+closedir($DIR);
 return @rv;
 }
 
 # create_atjob(user, time, commands, directory, send-email)
 sub create_atjob
 {
-local @tm = localtime($_[1]);
-local $date = strftime "%H:%M %b %d", @tm;
-local $mailflag = $_[4] ? "-m" : "";
-local $cmd = "cd ".quotemeta($_[3])." ; at $mailflag $date";
-local @uinfo = getpwnam($_[0]);
+my @tm = localtime($_[1]);
+my $date = strftime "%H:%M %b %d", @tm;
+my $mailflag = $_[4] ? "-m" : "";
+my $cmd = "cd ".quotemeta($_[3])." ; at $mailflag $date";
+my @uinfo = getpwnam($_[0]);
 if ($uinfo[2] != $<) {
 	# Only SU if we are not already the user
 	$cmd = &command_as_user($_[0], 0, $cmd);
 	}
+no strict "subs";
 &open_execute_command(AT, "$cmd >/dev/null 2>&1", 0); 
 print AT $_[2];
 close(AT);
+use strict "subs";
 &additional_log('exec', undef, $cmd);
 }
 
