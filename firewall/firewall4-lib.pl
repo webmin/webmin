44c083668bb9a25238f795092be3c05b77d19a85
Kay Marquardt (Gnadelwartz) <kay@rrr.de>
2017-06-21 10:15:15 +0200

    IPV4 use unified lin now :-)

---
+++ firewall/firewall4-lib.pl
@@ -0,0 +1,53 @@
+# firewall4-lib.pl
+# Functions for parsing iptables-save format files
+# include firewall-lib.pl, init, set IP version
+
+BEGIN { push(@INC, ".."); };
+use WebminCore;
+&init_config();
+
+# include common functions for ipv4 and ipv6
+require './firewall-lib.pl';
+
+# ipv4 initialization
+if ($config{'save_file'}) {
+	# Force use of a different save file, and webmin's functions
+	$iptables_save_file = $config{'save_file'};
+	}
+else {
+	if (-r "$module_root_directory/$gconfig{'os_type'}-lib.pl") {
+		# Use the operating system's save file and functions
+		do "$gconfig{'os_type'}-lib.pl";
+		}
+
+	if (!$iptables_save_file) {
+		# Use webmin's own save file
+		$iptables_save_file = "$module_config_directory/iptables.save";
+		}
+	}
+
+%access = &get_module_acl();
+
+@known_tables = ( "filter", "mangle", "nat" );
+@known_args =   ('-p', '-m', '-s', '-d', '-i', '-o', '-f',
+		 '--dport', '--sport', '--tcp-flags', '--tcp-option',
+		 '--icmp-type', '--mac-source', '--limit', '--limit-burst',
+		 '--ports', '--uid-owner', '--gid-owner',
+		 '--pid-owner', '--sid-owner', '--state', '--tos', '-j',
+		 '--to-ports', '--to-destination', '--to-source',
+		 '--reject-with', '--dports', '--sports',
+		 '--comment',
+		 '--physdev-is-bridged',
+		 '--physdev-is-in',
+		 '--physdev-is-out',
+		 '--physdev-in',
+		 '--physdev-out');
+
+# set IP Version
+&set_ipvx_version('ipv4');
+
+# IP V4 only functions
+# none :-)
+
+1;
+
