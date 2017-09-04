96c462ac0da7ef46ad5228085383c5acc6b73ce2
Jamie Cameron <jcameron@webmin.com>
2017-09-04 13:14:33 -0700

    Catalan update

--- bind8/system_info.pl
+++ bind8/system_info.pl
@@ -5,13 +5,15 @@ sub list_system_info
 {
 my ($data, $in) = @_;
 my @rv;
-my $err = &check_dnssec_client();
-if ($err) {
-	push(@rv, { 'type' => 'html',
-		    'open' => 1,
-		    'id' => $module_name.'_dnssec',
-		    'priority' => 100,
-		    'html' => $err });
+if (&foreign_available($module_name) && $access{'defaults'}) {
+	my $err = &check_dnssec_client();
+	if ($err) {
+		push(@rv, { 'type' => 'html',
+			    'open' => 1,
+			    'id' => $module_name.'_dnssec',
+			    'priority' => 100,
+			    'html' => $err });
+		}
 	}
 return @rv;
 }
