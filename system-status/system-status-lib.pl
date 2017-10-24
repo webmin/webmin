7fa1927121f4a442864bf008fd28eb4a89fe3e68
Jamie Cameron <jcameron@webmin.com>
2017-10-24 16:32:22 -0700

    Number of columns in vmstat output on FreeBSD isn't constant, but the three at always CPU user, kernel and idle time https://github.com/webmin/webmin/issues/659

--- system-status/system-status-lib.pl
+++ system-status/system-status-lib.pl
@@ -92,7 +92,7 @@ if ($gconfig{'os_type'} =~ /-linux$/ || $gconfig{'os_type'} eq 'freebsd') {
 		else {
 			# BSD format
 			# CPU user, kernel, idle
-			$info->{'cpu'} = [ $w[14], $w[15], $w[16], 0, 0 ];
+			$info->{'cpu'} = [ $w[-3], $w[-2], $w[-1], 0, 0 ];
 			}
 		}
 	}
