80510b63f61abef99f627fca7d1abac39aa38ec8
Jamie Cameron <jcameron@webmin.com>
2017-12-24 16:54:35 -0800

    fix bogomips regexp

--- proc/linux-lib.pl
+++ proc/linux-lib.pl
@@ -309,8 +309,9 @@ elsif ($c{'cache size'} =~ /^(\d+)\s+MB/i) {
 	$c{'cache size'} = $1*1024*1024;
 	}
 if (!$c{'cpu mhz'} && $c{'model name'}) {
-    $c{'model name'} .= " @ ".$c{'bogomips'}=~s/.$//r." bMips";
-}
+	$c{'bogomips'} =~ s/\..*$//;
+	$c{'model name'} .= " @ ".$c{'bogomips'}." bMips";
+	}
 
 if ($c{'model name'}) {
 	return ( $load[0], $load[1], $load[2],
