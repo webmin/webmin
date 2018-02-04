4c6020648877187a4dbf7b70bf0bed71d855b266
Jamie Cameron <jcameron@webmin.com>
2018-02-04 15:08:21 -0800

    Extract language-specific module descriptions into separate files, for acl module

---
+++ acl/module.info.es
@@ -0,0 +1,2 @@
+desc_es=Usuarios de Webmin
+longdesc_es=Crear usuarios de webmin y configurar a que modulos y caracteristicas pueden acceder.
---
+++ acl/module.info.pl
@@ -0,0 +1,2 @@
+desc_pl=U¿ytkownicy Webmina
+longdesc_pl=Twórz u¿ytkowników Webmina i kontroluj, które modu³y i funkcje s± dla nich dostêpne.
