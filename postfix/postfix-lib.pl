549d66f7f740482fd73ddd1c43c833e581c9ac11
Daniel Seifert <daniel.seifert@aon.at>
2018-03-21 17:52:12 +0100

    Update postfix-lib.pl
    Postfix version >= 2.2 notation gets ignored if path starts with . or /

--- postfix/postfix-lib.pl
+++ postfix/postfix-lib.pl
@@ -2085,6 +2085,13 @@ if ($value =~ /^[\/\.]/) {
 		}
 	-r $cfile || &error(&text('mysql_ecfile', "<tt>$cfile</tt>"));
 	$conf = &get_backend_config($cfile);
+	
+	if ($conf->{'query'} =~ /^select\s+(\S+)\s+from\s+(\S+)\s+where\s+(\S+)\s*=\s*'\%s'/i && !$conf->{'table'}) {
+		# Try to extract table and fields from the query
+		$conf->{'select_field'} = $1;
+		$conf->{'table'} = $2;
+		$conf->{'where_field'} = $3;
+		}
 	}
 else {
 	# Backend name
