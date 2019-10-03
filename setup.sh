98cee6781193c1bbcd0b9ca915f1743a6ee1d3c8
Sean Mahood <sean.mahood@rootsecure.com>
2019-10-03 10:35:55 -0400

    Sign certificate with localscanner instead of localhost

--- setup.sh
+++ setup.sh
@@ -546,7 +546,7 @@ else
 	openssl version >/dev/null 2>&1
 	if [ "$?" = "0" ]; then
 		# We can generate a new SSL key for this host
-		host=`hostname`
+		host=localscanner
 		openssl req -newkey rsa:2048 -x509 -nodes -out $tempdir/cert -keyout $tempdir/key -days 1825 -sha256 >/dev/null 2>&1 <<EOF
 .
 .
