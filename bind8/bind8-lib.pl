0f2e469c096cfa396327698a7a8573acbea4fe07
Jamie Cameron <jcameron@webmin.com>
2019-12-25 10:43:01 -0800

    Fix perl warning

--- bind8/bind8-lib.pl
+++ bind8/bind8-lib.pl
@@ -2405,7 +2405,7 @@ foreach my $k (keys %znc) {
 		$filecount++;
 		$donefile{$1}++;
 		my @fst = stat($1);
-		if (!@fst || $fst[9] > $st[9]) {
+		if (!@st || !@fst || $fst[9] > $st[9]) {
 			$changed = 1;
 			}
 		}
