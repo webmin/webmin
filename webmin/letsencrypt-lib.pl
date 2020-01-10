6382bc8f7ebda0be5c8c37749abcd691dbca2bc0
Jamie Cameron <jcameron@webmin.com>
2020-01-09 17:00:13 -0800

    Wildcard cert paths contain a *

--- webmin/letsencrypt-lib.pl
+++ webmin/letsencrypt-lib.pl
@@ -206,7 +206,7 @@ if ($letsencrypt_cmd) {
 		return (0, "<pre>".&html_escape($out || "No output from $letsencrypt_cmd")."</pre>");
 		}
 	my ($full, $cert, $key, $chain);
-	if ($out =~ /(\/etc\/letsencrypt\/(?:live|archive)\/[a-zA-Z0-9\.\_\-\/\r\n ]*\.pem)/) {
+	if ($out =~ /(\/etc\/letsencrypt\/(?:live|archive)\/[a-zA-Z0-9\.\_\-\/\r\n\* ]*\.pem)/) {
 		# Output contained the full path
 		$full = $1;
 		$full =~ s/\s//g;
