a3ce28e1ed1bcc6b3d2d38e1c41d763c0a1203df
Ilia Rostovtsev <ilia@rostovtsev.io>
2020-06-28 05:04:53 +0300

    Fix JSON 1/2

--- web-lib-funcs.pl
+++ web-lib-funcs.pl
@@ -15,7 +15,6 @@ Example code:
 ##use warnings;
 use Socket;
 use POSIX;
-use Encode;
 eval "use Socket6";
 $ipv6_module_error = $@;
 our $error_handler_funcs = [ ];
@@ -11004,7 +11003,6 @@ return 0;
 Converts the given Perl data structure to encoded binary string
 
 =item data parameter is a hash/array reference
-=item if the string should be UTF-8 encoded - most of the times, we already have UTF-8 on input, so don't use it
 =item if the output should be prettified
 
 =cut
@@ -11012,18 +11010,12 @@ sub convert_to_json
 {
 eval "use JSON::PP";
 if (!$@) {
-	my ($data, $utf8, $pretty) = @_;
+	my ($data, $pretty) = @_;
 	my $json = JSON::PP->new;
-	$utf8 = 0 if (!$utf8);
 	$pretty = 0 if (!$pretty);
 	$json = $json->pretty($pretty);
 	$data ||= {};
-	if	($utf8) {
-		return $json->encode($data);
-		} 
-	else {
-		return decode "UTF-8", $json->encode($data);
-		}
+	return $json->latin1->encode($data);
 	}
 else {
 	error("The JSON::PP Perl module is not available on your system : $@");
