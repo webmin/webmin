ad5d1d46130e2c1ae044d77b76bb64792e9173ea
Jamie Cameron <jcameron@webmin.com>
2020-12-20 19:29:55 -0800

    Improve error if module to require simply doesn't exist

--- web-lib-funcs.pl
+++ web-lib-funcs.pl
@@ -3679,6 +3679,7 @@ my @OLDINC = @INC;
 my $mdir = &module_root_directory($mod);
 $mdir =~ /^(.*)$/; # untaint, part 1
 $mdir = $1; 	   # untaint, part 2
+$mdir && -d $mdir || &error("Module $mod does not exist");
 @INC = &unique($mdir, @INC);
 -d $mdir || &error("Module $mod does not exist");
 if (!&get_module_name() && $mod) {
