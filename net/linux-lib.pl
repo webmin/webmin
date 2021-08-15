e39b67c98c1e5915dd982b1d044263d8632e4b3c
Jamie Cameron <jcameron@webmin.com>
2021-08-14 17:53:37 -0700

    Support 'secondary' IP keyword https://github.com/webmin/webmin/issues/1506

--- net/linux-lib.pl
+++ net/linux-lib.pl
@@ -56,7 +56,7 @@ if (&has_command("ip")) {
 			$ifc{'address'} = $1;
 			$ifc{'netmask'} = &prefix_to_mask("$3");
 			}
-		elsif ($l =~ /\sinet\s+([0-9\.]+)\/(\d+)(\s+brd\s+(\S+))?\s+scope\s+global(\s+(noprefixroute|dynamic))*\s+(\S+)/ && $7 eq $ifc{'name'}) {
+		elsif ($l =~ /\sinet\s+([0-9\.]+)\/(\d+)(\s+brd\s+(\S+))?\s+scope\s+global(\s+(noprefixroute|dynamic|secondary))*\s+(\S+)/ && $7 eq $ifc{'name'}) {
 			# Line like :
 			# inet 193.9.101.120/24 brd 193.9.101.255 scope global br0
 			$ifc{'address'} = $1;
