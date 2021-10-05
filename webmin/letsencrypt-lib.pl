9032381882fca4616f31152eb8b5caf3a0a12395
Ilia Rostovtsev <ilia@rostovtsev.io>
2021-10-05 18:21:33 +0300

    Update LE IC R3+R4 https://letsencrypt.org/certificates/ https://forum.virtualmin.com/t/intermediate-certificate-failure-blocks-dovecot/112492/7

--- webmin/letsencrypt-lib.pl
+++ webmin/letsencrypt-lib.pl
@@ -13,7 +13,8 @@ else {
 $account_key = "$module_config_directory/letsencrypt.pem";
 
 $letsencrypt_chain_urls = [
-	"https://letsencrypt.org/certs/lets-encrypt-r3-cross-signed.pem",
+	"https://letsencrypt.org/certs/lets-encrypt-r3.pem",
+	"https://letsencrypt.org/certs/lets-encrypt-r4.pem",
 	];
 
 # check_letsencrypt()
