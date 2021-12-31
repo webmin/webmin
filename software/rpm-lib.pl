c29682afc7bef673d5d9f153b7b80093d4fb5ee7
Jamie Cameron <jcameron@webmin.com>
2021-12-31 12:28:22 -0800

    Better handle case where some requested packages don't exist

--- software/rpm-lib.pl
+++ software/rpm-lib.pl
@@ -14,7 +14,9 @@ local($i, $list); $i = 0;
 $list = @_ ? join(' ', map { quotemeta($_) } @_) : "-a";
 %packages = ( );
 &open_execute_command(RPM, "rpm -q $list --queryformat \"%{NAME}\\n%{VERSION}-%{RELEASE}\\n%{EPOCH}\\n%{GROUP}\\n%{ARCH}\\n%{SUMMARY}\\n\\n\"", 1, 1);
-while($packages{$i,'name'} = <RPM>) {
+while(my $name = <RPM>) {
+	next if ($name =~ /is\s+not\s+installed$/);
+	$packages{$i,'name'} = $name;
 	chop($packages{$i,'name'});
 	chop($packages{$i,'version'} = <RPM>);
 	chop($packages{$i,'epoch'} = <RPM>);
