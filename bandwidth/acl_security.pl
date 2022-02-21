491a91dbfcbdeb138670e56678cef64a9f72febd
Jamie Cameron <jcameron@webmin.com>
2022-02-20 19:51:06 -0800

    Use ui-lib

--- bandwidth/acl_security.pl
+++ bandwidth/acl_security.pl
@@ -5,17 +5,16 @@ do 'bandwidth-lib.pl';
 # Output HTML for editing security options for the acl module
 sub acl_security_form
 {
-print "<tr> <td><b>$text{'acl_setup'}</b></td> <td>\n";
-printf "<input type=radio name=setup value=1 %s> $text{'yes'}\n",
-	$o->{'setup'} ? 'checked' : '';
-printf "<input type=radio name=setup value=0 %s> $text{'no'}</td> </tr>\n",
-	$o->{'setup'} ? '' : 'checked';
+my ($o) = @_;
+print &ui_table_row($text{'acl_setup'},
+	&ui_yesno_radio("setup", $o->{'setup'}));
 }
 
 # acl_security_save(&options)
 # Parse the form for security options for the acl module
 sub acl_security_save
 {
-$_[0]->{'setup'} = $in{'setup'};
+my ($o) = @_;
+$o->{'setup'} = $in{'setup'};
 }
 
