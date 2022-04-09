31505d31361eea21ba985de5e2cfdda0c3ee763e
Jamie Cameron <jcameron@webmin.com>
2022-04-09 13:19:27 -0700

    Don't show as installed if firewalld is in use

--- firewall/install_check.pl
+++ firewall/install_check.pl
@@ -12,6 +12,7 @@ return 0 if (&missing_firewall_commands());
 local $out = &backquote_command("iptables -n -t filter -L OUTPUT 2>&1");
 return 0 if ($?);
 if ($_[0]) {
+	# Check if init script is setup
 	if (!$config{'direct'} &&
 	    (defined(&check_iptables) && &check_iptables() ||
 	     !-s $iptables_save_file)) {
@@ -19,6 +20,11 @@ if ($_[0]) {
 		}
 	return 2;
 	}
+# Check if another firewall is in use
+my @livetables = &get_iptables_save("ip${ipvx}tables-save 2>/dev/null |");
+my @fwname = &external_firewall_list(\@livetables);
+@fwname = grep { $_ ne 'fail2ban' } @fwname;
+return 0 if (@fwname);
 return 1;
 }
 
