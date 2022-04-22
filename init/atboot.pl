05ed0be5730672d2900b9490492265c1e42b6e5b
Ilia <gpg@ilia.engineer>
2022-04-23 01:06:24 +0300

    Fix `systemd` script generation for manual installs

--- init/atboot.pl
+++ init/atboot.pl
@@ -117,8 +117,22 @@ elsif ($init_mode eq "win32") {
 	$perl_path = &get_perl_path();
 	&enable_at_boot($product, $ucproduct, $perl_path." ".&quote_path("$root_directory/miniserv.pl")." ".&quote_path("$config_directory/miniserv.conf"));
 	}
-elsif ($init_mode eq "rc" || $init_mode eq "upstart" ||
-       $init_mode eq "systemd") {
+elsif ($init_mode eq "systemd") {
+	# Create systemd
+	&enable_at_boot(
+	     $product,
+	     "$ucproduct server daemon",
+	     "$config_directory/start-init",
+	     "$config_directory/stop-init",
+	     undef,
+	       { 'pidfile'  => $var_directory."/miniserv.pid",
+	         'opts' => {
+	         'type'    => 'forking',
+	         'killmode'   => 'none'
+	       }},
+	     );
+	}
+elsif ($init_mode eq "rc" || $init_mode eq "upstart") {
 	# Create RC or upstart script
 	&enable_at_boot($product, $ucproduct, "$config_directory/start",
 			"$config_directory/stop",
