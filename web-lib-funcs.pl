e01579dfd1daf9305084b57f23d8af8c6a533991
Jamie Cameron <jcameron@webmin.com>
2022-07-01 21:59:10 -0700

    javascript: can use different HTML entities

--- web-lib-funcs.pl
+++ web-lib-funcs.pl
@@ -8644,8 +8644,8 @@ sub filter_javascript
 my ($rv) = @_;
 $rv =~ s/<\s*script[^>]*>([\000-\377]*?)<\s*\/script\s*>//gi;
 $rv =~ s/(on(Abort|BeforeUnload|Blur|Change|Click|ContextMenu|Copy|Cut|DblClick|Drag|DragEnd|DragEnter|DragLeave|DragOver|DragStart|DragDrop|Drop|Error|Focus|FocusIn|FocusOut|HashChange|Input|Invalid|KeyDown|KeyPress|KeyUp|Load|MouseDown|MouseEnter|MouseLeave|MouseMove|MouseOut|MouseOver|MouseUp|Move|Paste|PageShow|PageHide|Reset|Resize|Scroll|Search|Select|Submit|Toggle|Unload)=)/x$1/gi;
-$rv =~ s/(javascript:)/x$1/gi;
-$rv =~ s/(vbscript:)/x$1/gi;
+$rv =~ s/(javascript(:|&colon;|&#58;|&#x3A;))/x$1/gi;
+$rv =~ s/(vbscript(:|&colon;|&#58;|&#x3A;))/x$1/gi;
 $rv =~ s/<([^>]*\s|)(on\S+=)(.*)>/<$1x$2$3>/gi;
 return $rv;
 }
