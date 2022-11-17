a57af8c48151843a75969d31eb4b112c1dbdaa5e
iliajie <gpg@ilia.engineer>
2022-11-17 15:49:45 +0200

    Add missing `vendor_perl` to include

--- makedist.pl
+++ makedist.pl
@@ -64,7 +64,7 @@ else {
 	close($fh);
 	@mlist = split(/\s+/, $mod_def_list);
 	}
-@dirlist = ( "WebminUI" );
+@dirlist = ( "WebminUI", "vendor_perl" );
 
 $dir = "webmin-$vers";
 if (!$release || !-d "$tardir/$dir") {
