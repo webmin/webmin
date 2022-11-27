e58d54ed86cdaad4810c7e24086cc0ba721c10cb
Jamie Cameron <jcameron@webmin.com>
2022-11-27 12:23:23 -0800

    Use absolute path

--- xterm/xterm-lib.pl
+++ xterm/xterm-lib.pl
@@ -4,6 +4,6 @@ BEGIN { push(@INC, ".."); };
 use WebminCore;
 &init_config();
 our %access = &get_module_acl();
-do 'websockets-lib-funcs.pl';
+do "$module_root_directory/websockets-lib-funcs.pl";
 
 1;
