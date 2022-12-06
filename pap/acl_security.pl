ce43b53e5cb9bd5f8c924375d4459493982ac5ea
Jamie Cameron <jcameron@webmin.com>
2022-12-05 23:20:23 -0800

    ui-lib conversion

--- pap/acl_security.pl
+++ pap/acl_security.pl
@@ -5,28 +5,28 @@ require 'pap-lib.pl';
 # Output HTML for editing security options for the pap module
 sub acl_security_form
 {
-print "<tr> <td valign=top><b>$text{'acl_pages'}</b></td> <td colspan=3>\n";
-foreach $a ('mgetty', 'options', 'dialin', 'secrets', 'sync') {
-	print "&nbsp;&nbsp;\n" if ($a eq 'sync');
-	printf "<input type=checkbox name=%s value=1 %s> %s<br>\n",
-		$a, $_[0]->{$a} ? "checked" : "", $text{$a."_title"};
-	}
-print "</td> </tr>\n";
+my ($o) = @_;
 
-print "<tr> <td><b>$text{'acl_direct'}</b></td>\n";
-printf "<td><input type=radio name=direct value=1 %s> %s\n",
-	$_[0]->{'direct'} ? "checked" : "", $text{'yes'};
-printf "<input type=radio name=direct value=0 %s> %s</td> </tr>\n",
-	$_[0]->{'direct'} ? "" : "checked", $text{'no'};
+print &u_table_row($text{'acl_pages'},
+    &ui_checkbox('mgetty', 1, $text{'mgetty_title'}, $o->{'mgetty'})."<br>\n".
+    &ui_checkbox('options', 1, $text{'options_title'}, $o->{'options'})."<br>\n".
+    &ui_checkbox('dialin', 1, $text{'dialin_title'}, $o->{'dialin'})."<br>\n".
+    &ui_checkbox('secrets', 1, $text{'secrets_title'}, $o->{'secrets'})."<br>\n".
+    &ui_checkbox('sync', 1, $text{'sync_title'}, $o->{'sync'}), 3);
+
+print &ui_table_row($text{'acl_direct'},
+	&ui_yesno_radio("direct", $o->{'direct'}));
 }
 
 # acl_security_save(&options)
 # Parse the form for security options for the squid module
 sub acl_security_save
 {
+my ($o) = @_;
+
 foreach $a ('mgetty', 'options', 'dialin', 'secrets', 'sync') {
-	$_[0]->{$a} = $in{$a};
+	$o->{$a} = $in{$a};
 	}
-$_[0]->{'direct'} = $in{'direct'};
+$o->{'direct'} = $in{'direct'};
 }
 
