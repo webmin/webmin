f46cc45c0d038ee8d43c2af37f4ef7b23e8e997e
Jamie Cameron <jcameron@webmin.com>
2023-02-04 15:22:56 -0800

    Don't use array if it doesn't exist

--- web-lib-funcs.pl
+++ web-lib-funcs.pl
@@ -324,7 +324,7 @@ my $tmp_base = $gconfig{'tempdir_'.&get_module_name()} ?
 		  $ENV{'TMP'} && $ENV{'TMP'} ne "/tmp" ? $ENV{'TMP'} :
 		  -d "c:/temp" ? "c:/temp" : "/tmp/.webmin";
 my $tmp_dir;
-if (-d $remote_user_info[7] && !$gconfig{'nohometemp'}) {
+if (@remote_user_info && -d $remote_user_info[7] && !$gconfig{'nohometemp'}) {
 	$tmp_dir = "$remote_user_info[7]/.tmp";
 	}
 elsif (@remote_user_info) {
