b5aaba91198e540e7447b0224192dc67bbd4d912
Jamie Cameron <jcameron@webmin.com>
2023-03-03 21:37:10 -0800

    What are roles??

--- webmin/webmin-lib.pl
+++ webmin/webmin-lib.pl
@@ -1241,9 +1241,8 @@ my %rdisallow = map { $_, 1 } split(/\s+/, $raccess{'disallow'} || "");
 my %access = &get_module_acl();
 my %disallow = map { $_, 1 } split(/\s+/, $access{'disallow'} || "");
 my %allow = map { $_, 1 } split(/\s+/, $access{'allow'} || "");
-my %role = map { $_, 1 } split(/\s+/, $access{'role'} || "");
 if (&foreign_available($module_name) && !$gconfig{'nowebminup'} &&
-    !$noupdates && ($allow{'upgrade'} || $role{'upgrader'} ||
+    !$noupdates && ($allow{'upgrade'} ||
 		    (!$disallow{'upgrade'} && !$rdisallow{'upgrade'}))) {
 	if (!$config{'last_version_check'} ||
 	    $now - $config{'last_version_check'} > 24*60*60) {
