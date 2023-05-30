8cd0b8ab49a2e5ac8451f686f58f300c7b99c0a3
iliajie <gpg@ilia.engineer>
2023-05-30 12:46:17 +0300

    Fix to allow `allow-same-origin` and `allow-popups-to-escape-sandbox`

--- mailboxes/folders-lib.pl
+++ mailboxes/folders-lib.pl
@@ -2865,7 +2865,9 @@ if ($body =~ /<\/body>/) {
 		}
 $body = &trim(&quote_escape($body, '"'));
 my $iframe_body = $iframe_spinner.
-      "<iframe sandbox id='mail-iframe' style='border:0; width:100%; min-height:50vh;'  
+      "<iframe
+         sandbox='allow-same-origin allow-popups-to-escape-sandbox'
+         id='mail-iframe' style='border:0; width:100%; min-height:50vh;'  
          onload='this.style.height=this.contentWindow.document.body.getBoundingClientRect().bottom +
                  this.contentWindow.document.body.getBoundingClientRect().top + \"px\";
                    this.previousElementSibling.remove();this.classList.add(\"loaded\")' 
