ff42c6612934fe4f9bdb95671456ef4de5517209
iliajie <gpg@ilia.engineer>
2023-07-22 22:20:47 +0300

    Fix to drop using `official` word

--- setup-repos.sh
+++ setup-repos.sh
@@ -92,7 +92,7 @@ fi
 
 # Ask first
 if [ "$1" != "-f" ] && [ "$1" != "--force" ]; then
-  printf "Setup Webmin official repository? (y/N) "
+  printf "Setup Webmin repository? (y/N) "
   read -r sslyn
   if [ "$sslyn" != "y" ] && [ "$sslyn" != "Y" ]; then
     exit
