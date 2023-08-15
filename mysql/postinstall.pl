57e710e8a621504cc4d6ca775d8d0f43d919ed87
Jamie Cameron <jcameron@webmin.com>
2023-08-14 23:02:35 -0700

    Small code cleanup

--- mysql/postinstall.pl
+++ mysql/postinstall.pl
@@ -4,7 +4,7 @@ require 'mysql-lib.pl';
 sub module_install
 {
 my $mysql_version = &get_mysql_version();
-if ($mysql_version > 0) {
+if ($mysql_version && $mysql_version >= 0) {
 	&save_mysql_version($mysql_version);
 	&create_module_info_overrides();
 	}
