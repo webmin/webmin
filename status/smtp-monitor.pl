0704ac640e93c3ab80741ec204b7ce30aef934d5
Jamie Cameron <jcameron@webmin.com>
2024-03-11 20:39:59 -0700

    Start of work on SMTP monitor

--- status/smtp-monitor.pl
+++ status/smtp-monitor.pl
@@ -6,9 +6,10 @@ my ($serv) = @_;
 
 &foreign_require("mailboxes");
 $main::error_must_die = 1;
+my $desc;
 eval {
 	my $h = { 'fh' => 'MAIL' };
-        &open_socket($server, $port, $h->{'fh'});
+        &open_socket($serv->{'host'}, $serv->{'port'}, $h->{'fh'});
 	if ($serv->{'ssl'} == 1) {
                 # Switch to SSL mode right now
                 &switch_smtp_to_ssl($h);
@@ -27,15 +28,27 @@ eval {
                         $serv->{'ssl'} = 0;
                         }
                 }
-	# XXX
+	$desc = $text{'smtp_ok1'};
+
+	# Open an SMTP transaction
+	if ($serv->{'from'}) {
+		&mailboxes::smtp_command($h, "mail from: <$serv->{'from'}>\r\n");
+		$desc = $text{'smtp_ok2'};
+		}
+	if ($serv->{'to'}) {
+		&mailboxes::smtp_command($h, "rcpt to: <$serv->{'to'}>\r\n");
+		$desc = $text{'smtp_ok3'};
+		}
+	&mailboxes::smtp_command($h, "quit\r\n");
+	&close_http_connection($h);
 	};
 if ($@) {
-        $err = &entities_to_ascii(&html_tags_to_text("$@"));
+        $err = &entities_to_ascii("$@");
         $err =~ s/at\s+\S+\s+line\s+\d+.*//;
 	return { 'up' => 0,
 		 'desc' => $err };
 	}
-return { 'up' => 1 };
+return { 'up' => 1, 'desc' => $desc };
 }
 
 sub switch_smtp_to_ssl
@@ -62,10 +75,24 @@ print &ui_table_row($text{'smtp_host'},
 	&ui_textbox("host", $serv->{'host'}, 25));
 
 print &ui_table_row($text{'smtp_port'},
-	&ui_textbox("port", $serv->{'port'}, 5));
+	&ui_textbox("port", $serv->{'port'} || 25, 5));
+
+print &ui_table_row($text{'smtp_ssl'},
+	&ui_radio("ssl", $serv->{'ssl'} || 0,
+		  [ [ 0, $text{'smtp_ssl0'} ],
+		    [ 1, $text{'smtp_ssl1'} ],
+		    [ 2, $text{'smtp_ssl2'} ] ]));
+
+print &ui_table_row($text{'smtp_from'},
+	&ui_opt_textbox("from", $serv->{'from'}, 25,
+			$text{'smtp_none'}, $text{'smtp_addr'}));
+
+print &ui_table_row($text{'smtp_to'},
+	&ui_opt_textbox("to", $serv->{'to'}, 25,
+			$text{'smtp_none'}, $text{'smtp_addr'}));
 }
 
-sub parse_tcp_dialog
+sub parse_smtp_dialog
 {
 my ($serv) = @_;
 
@@ -75,6 +102,24 @@ $serv->{'host'} = $in{'host'};
 
 $in{'port'} =~ /^\d+$/ || &error($text{'smtp_eport'});
 $serv->{'port'} = $in{'port'};
+
+$serv->{'ssl'} = $in{'ssl'};
+
+if ($in{'from_def'}) {
+	delete($serv->{'from'});
+	}
+else {
+	$in{'from'} =~ /^\S+\@\S+$/ || &error($text{'smtp_efrom'});
+	$serv->{'from'} = $in{'from'};
+	}
+
+if ($in{'to_def'}) {
+	delete($serv->{'to'});
+	}
+else {
+	$in{'to'} =~ /^\S+\@\S+$/ || &error($text{'smtp_eto'});
+	$serv->{'to'} = $in{'to'};
+	}
 }
 
 1;
