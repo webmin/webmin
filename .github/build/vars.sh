d1f0e77d25ef98c055e341776216311766086691
Ilia Ross <ilia@ross.gdn>
2024-04-13 22:30:04 +0300

    Dev: What's up this time? [build]

--- .github/build/vars.sh
+++ .github/build/vars.sh
@@ -18,10 +18,3 @@ cloud_upload_ssh_host="${ENV_BUILD__CLOUD_UPLOAD_SSH_HOST:-webmin.dev}"
 cloud_upload_ssh_dir="${ENV_BUILD__CLOUD_UPLOAD_SSH_DIR:-~/domains/builds.webmin.dev/public_html}"
 cloud_upload_gpg_passphrase="${WEBMIN_DEV__GPG_PH}"
 
-# Define verbosity level
-verbosity_level=' >/dev/null 2>&1 </dev/null'
-verbosity_level_to_file='2> /dev/null'
-verbosity_level_with_input=' >/dev/null 2>&1'
-if [[ "'$*'" == *"--debug"* ]]; then
-    unset verbosity_level verbosity_level_to_file verbosity_level_with_input
-fi
