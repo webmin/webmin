5dc7cfafd7a95444c128ee8e2d6ade1383960b54
Ilia Ross <ilia@ross.gdn>
2024-06-12 20:14:06 +0300

    Fix to allow disabling the enforcement of sudo-capable logins

--- xterm/acl_security.pl
+++ xterm/acl_security.pl
@@ -10,6 +10,14 @@ my ($o) = @_;
 print &ui_table_row($text{'acl_user'},
 	&ui_opt_textbox("user", $o->{'user'} eq '*' ? undef : $o->{'user'},
 			20, $text{'acl_sameuser'}));
+
+if ($o->{'user'} eq "root" && $remote_user ne $o->{'user'}) {
+	print &ui_table_row($text{'acl_sudoenforce'},
+		&ui_yesno_radio("sudoenforce", $o->{'sudoenforce'} == 1 ? 1 : 0));
+	}
+else {
+	print &ui_hidden("sudoenforce", $o->{'sudoenforce'});
+	}
 }
 
 sub acl_security_save
@@ -17,4 +25,5 @@ sub acl_security_save
 my ($o) = @_;
 
 $o->{'user'} = $in{'user_def'} ? '*' : $in{'user'};
+$o->{'sudoenforce'} = $in{'sudoenforce'} ? 1 : 0;
 }
