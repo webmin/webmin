a6e0b3cee5bf69ab722d902a192b70029f6be509
Ilia Ross <ilia@ross.gdn>
2024-11-16 14:03:39 +0200

    Update translations and include the new Polish translation

--- fetchmail/module.info.pl
+++ fetchmail/module.info.pl
@@ -1,2 +1,2 @@
-desc_pl=Fetchmail  - pobieranie poczty
-longdesc_pl=Konfiguracja popularnego programu fetchmail do automatycznego pobierania e-maili z innych serwerów
+longdesc_pl=Skonfiguruj Fetchmail, aby pobierać wiadomości e-mail z zdalnego serwera POP3 lub IMAP na lokalny adres.
+desc_pl=Odbieranie Poczty Fetchmail
--- quota/module.info.pl
+++ quota/module.info.pl
@@ -1,1 +1,2 @@
-desc_pl=Przydziały (Quota) dysków
+longdesc_pl=Wyświetl swoje limity dyskowe oraz limity grup na wszystkich systemach plików, dla których są one włączone.
+desc_pl=Limity dyskowe
