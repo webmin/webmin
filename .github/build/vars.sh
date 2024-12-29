416c614d6c39e0d0866736f9caac2b5630bb4402
Ilia Ross <ilia@ross.gdn>
2024-12-29 14:47:09 +0200

    Fix to update moved home for webmin.dev testing builds

--- .github/build/vars.sh
+++ .github/build/vars.sh
@@ -18,7 +18,7 @@ github_token="${ENV_BUILD__GITHUB_TOKEN}"
 # Cloud upload config
 cloud_upload_ssh_user="${ENV_BUILD__CLOUD_UPLOAD_SSH_USER:-webmin.dev}"
 cloud_upload_ssh_host="${ENV_BUILD__CLOUD_UPLOAD_SSH_HOST:-webmin.dev}"
-cloud_upload_ssh_dir="${ENV_BUILD__CLOUD_UPLOAD_SSH_DIR:-~/domains/builds.webmin.dev/public_html}"
+cloud_upload_ssh_dir="${ENV_BUILD__CLOUD_UPLOAD_SSH_DIR:-~/domains/download.webmin.dev/public_html}"
 cloud_upload_gpg_passphrase="${WEBMIN_DEV__GPG_PH}"
 
 # Define verbosity level
