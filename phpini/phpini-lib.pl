615838be9e313e1a065adac895b32fc8f7b509fc
Ilia Ross <ilia@ross.gdn>
2025-02-10 14:09:26 +0200

    Fix to test if under directory correctly

--- phpini/phpini-lib.pl
+++ phpini/phpini-lib.pl
@@ -505,7 +505,7 @@ if ($file && -r $file && &foreign_check("virtual-server")) {
 		my $conf;
 		my $filedir = $file;
 		$filedir =~ s/\/[^\/]+$//;
-		my @conf = grep { &is_under_directory($_->{'dir'}, $filedir) }
+		my @conf = grep { &is_under_directory($filedir, $_->{'dir'}) }
 			     &virtual_server::list_php_fpm_configs();
 		if (@conf) {
 			$conf = &virtual_server::get_php_fpm_config(
