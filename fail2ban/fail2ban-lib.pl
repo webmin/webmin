a80870980d3a4fffa683e7337754714399cdabaf
Ilia Ross <ilia@ross.gdn>
2025-04-30 22:32:41 +0300

    Fix to correctly merge custom `*.conf` files
    https://forum.virtualmin.com/t/fail2ban-intrusion-detector-firewalld-default-jail-options/133201/4?u=ilia

--- fail2ban/fail2ban-lib.pl
+++ fail2ban/fail2ban-lib.pl
@@ -83,16 +83,16 @@ if (-r $jfile) {
 	push(@rv, &parse_config_file($jfile));
 	}
 
+my @lrv;
 # Read separate config files under jail.d
 my $jdir = "$config{'config_dir'}/jail.d";
 if (-d $jdir) {
 	foreach my $f (glob("$jdir/*.conf")) {
-		push(@rv, &parse_config_file($f));
+		push(@lrv, &parse_config_file($f));
 		}
 	}
 
 # Read the main local file, and separate files under jail.d
-my @lrv;
 my $jlfile = &make_local_file($jfile);
 if (-r $jlfile) {
 	push(@lrv, &parse_config_file($jlfile));
