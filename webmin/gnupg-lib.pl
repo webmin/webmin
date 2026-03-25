e499b5b3a56ff265c0b310c88a401b3bb2161557
Ilia Ross <ilia@ross.gdn>
2026-03-25 18:30:13 +0200

    Fix to use `loopback pinentry` mode for GPG passphrase handling
    https://forum.virtualmin.com/t/gpg-encryption-in-usermin/136729/19?u=ilia

--- webmin/gnupg-lib.pl
+++ webmin/gnupg-lib.pl
@@ -284,7 +284,8 @@ if (!defined($pass)) {
 	return $text{'gnupg_esignpass'}.". ".
 	    &text('gnupg_canset', "/gnupg/edit_key.cgi?key=$key->{'key'}").".";
 	}
-my $pflag = "--batch --passphrase-file ".quotemeta(&get_passphrase_file($key));
+my $pflag = "--batch --pinentry-mode loopback --passphrase-file ".
+	    quotemeta(&get_passphrase_file($key));
 my $cmd;
 if ($mode == 0) {
 	$cmd = "$gpgpath $pflag --output ".quotemeta($dstfile)." --default-key $key->{'key'} --sign ".quotemeta($srcfile);
