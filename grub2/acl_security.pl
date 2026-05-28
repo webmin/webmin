440ddabef1c151cb15c2813f13a4d2d7a79350fa
Ilia Ross <ilia@ross.gdn>
2026-05-28 14:26:45 +0200

    Fix ACL page sections

--- grub2/acl_security.pl
+++ grub2/acl_security.pl
@@ -12,21 +12,16 @@ our (%in, %text);
 sub acl_security_form
 {
 my ($o) = @_;
-print &ui_table_span(&ui_tag('b', &html_escape($text{'acl_section_view'})));
-# View is separated because it controls whether the module can be entered.
-foreach my $a (qw(view)) {
-	print &ui_table_row($text{'acl_'.$a},
-			    &ui_yesno_radio($a, &grub2_check_acl($a, $o)), 3);
-	}
+# View permissions
+print &ui_table_row($text{'acl_view'},
+		    &ui_yesno_radio("view", &grub2_check_acl("view", $o)), 3);
 print &ui_table_hr();
-print &ui_table_span(&ui_tag('b', &html_escape($text{'acl_section_change'})));
 # Change permissions modify GRUB configuration without granting install rights.
 foreach my $a (qw(edit security apply runtime)) {
 	print &ui_table_row($text{'acl_'.$a},
 			    &ui_yesno_radio($a, &grub2_check_acl($a, $o)), 3);
 	}
 print &ui_table_hr();
-print &ui_table_span(&ui_tag('b', &html_escape($text{'acl_section_admin'})));
 # Admin permissions expose direct file editing, backup, and boot-loader install.
 foreach my $a (qw(manual install backup)) {
 	print &ui_table_row($text{'acl_'.$a},
