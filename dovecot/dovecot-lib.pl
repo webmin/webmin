42a5838d9e97d0a3bb6f31a1214cb9e7533a7f44
Ilia Ross <ilia@ross.gdn>
2026-06-04 22:35:42 +0200

    Fix not to expect option saved in config

--- dovecot/dovecot-lib.pl
+++ dovecot/dovecot-lib.pl
@@ -26,7 +26,7 @@ return undef;
 # directives
 sub get_add_config_file
 {
-my $f = $config{'add_config'};
+my $f = $config{'add_config'} || "";
 $f =~ s/^\s+|\s+$//g;
 if ($f) {
 	return $f if (-r $f);
