65e171a92ff46cc752617d39fbbc467acd918f65
Ilia Ross <ilia@ross.gdn>
2026-08-04 01:58:27 +0200

    Fix to use module config
    https://github.com/webmin/webmin/pull/2807#pullrequestreview-4849404584

--- quota/config_info.pl
+++ quota/config_info.pl
@@ -1,4 +1,4 @@
-# Build and parse the Btrfs-specific module configuration field.
+# Hide Btrfs-specific configuration when it cannot be used.
 require './quota-lib.pl';
 
 # config_pre_load(info, [order])
@@ -13,24 +13,6 @@ return if (@btrfs && &has_command("btrfs"));
 # Remove the field from both the configuration metadata and display order.
 delete($info->{'btrfs_mode'});
 @$order = grep { $_ ne "btrfs_mode" } @$order if ($order);
-}
-
-# show_btrfs_mode(mode)
-# Display the accounting mode selector, defaulting unknown values to full mode.
-sub show_btrfs_mode
-{
-my ($mode) = @_;
-$mode = "full" if ($mode ne "simple");
-return &ui_radio("btrfs_mode", $mode,
-	[ [ "full", $text{'config_btrfs_full'} ],
-	  [ "simple", $text{'config_btrfs_simple'} ] ]);
-}
-
-# parse_btrfs_mode()
-# Store only a supported mode and fall back to full accounting otherwise.
-sub parse_btrfs_mode
-{
-return $in{'btrfs_mode'} eq "simple" ? "simple" : "full";
 }
 
 1;
